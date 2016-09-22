8a6ad20c80740514a780da0f05c5ce54c6a33a5e
Thomas Mullen <thomasmullendesign@gmail.com>
2016-09-22 17:15:20 -0400

    BRAD

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -17,8 +17,7 @@ router.get('/', Auth.isLoggedIn, function(req, res, next) {
 /* Profile page
 -------------------------------------------------------------------------------- */
 router.get('/profile', Auth.isLoggedIn, function(req, res, next) {
-    req.user.name = Utils.escapeHtml(req.user.firstName);
-	res.render('dashboard/profile', {
+    res.render('dashboard/profile', {
 		dashboard : "ibm-highlight",
 		user : req.user,
 		section : 'profile'
@@ -412,7 +411,6 @@ router.get('/users', Auth.isAdmin, function(req, res, next) {
 
 			return;
 		}
-
 		res.render('dashboard/users', {
 			dashboard : "ibm-highlight",
 			user : req.user,
